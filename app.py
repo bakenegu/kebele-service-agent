d4e59ca08448811ee6fa4a117476c5ed77b6031e
Bakenegu502 <biruk.negu@globedocket.com>
2025-12-26 14:06:53 +0300

    Add application file

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from src.ui.gradio_app import launch
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo.launch()
 
 if __name__ == "__main__":
     launch()

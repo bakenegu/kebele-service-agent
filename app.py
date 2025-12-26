67a755de0c26ac62337e2cf9e38c1aa9956481c8
Bakenegu502 <biruk.negu@globedocket.com>
2025-12-26 14:11:21 +0300

    Fix app.py entrypoint for HF

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 from src.ui.gradio_app import launch
-import gradio as gr
-
-def greet(name):
-    return "Hello " + name + "!!"
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
 
 if __name__ == "__main__":
     launch()
